16cea8ecedc1ce60c11da787cc7c1218d01460c3
lyk990 <1477435414@qq.com>
2022-10-18 22:49:00 +0800

    effect

---
+++ src/reactivity/effect.ts
@@ -0,0 +1,40 @@
+class ReactiveEffect {
+  private _fn: any;
+  constructor(fn) {
+    this._fn = fn;
+  }
+  run() {
+    activeEffect = this;
+    this._fn();
+  }
+}
+
+const targetMap = new Map();
+export function track(target, key) {
+  //   target -> key -> dep
+  let depsMap = targetMap.get(target);
+  if (!depsMap) {
+    depsMap = new Map();
+    targetMap.set(target, depsMap);
+  }
+
+  let dep = depsMap.get(key);
+  if (!dep) {
+    dep = new Set();
+    depsMap.set(key, dep)
+  }
+  dep.add(activeEffect)
+  // const dep = new Set()
+}
+
+export function trigger(target, key) {
+  
+}
+
+let activeEffect;
+export function effect(fn) {
+  //  fn
+  const _effect = new ReactiveEffect(fn);
+
+  _effect.run();
+}
