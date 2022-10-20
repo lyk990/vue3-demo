9b148d7923887d6323ba92702888c56b09fcc1e2
lyk990 <1477435414@qq.com>
2022-10-20 23:55:15 +0800

    proxyRefs

--- src/reactivity/ref.ts
+++ src/reactivity/ref.ts
@@ -7,6 +7,7 @@ class RefImpl {
   private _value: any;
   public dep;
   private _rawValue: any;
+  private _v_isRef = true;
   constructor(value) {
     this._rawValue = value;
     // 查看value1是否是一个对象
@@ -40,4 +41,29 @@ function trackRefValue(ref) {
 
 export function ref(value) {
   return new RefImpl(value);
+}
+
+export function isRef(ref) {
+  //
+  return !!ref._v_isRef;
+}
+
+export function unRef(ref) {
+  // 看看是不是一个ref对象
+  return isRef(ref) ? ref.value : ref;
+}
+
+export function proxyRefs(objectWithRefs) {
+  return new Proxy(objectWithRefs, {
+    get(target, key) {
+      return unRef(Reflect.get(target, key));
+    },
+    set(target, key, value) {
+      if (isRef(target[key]) && !isRef(value)) {
+        return (target[key].value = value);
+      } else {
+        return Reflect.set(target, key, value);
+      }
+    },
+  });
 }
--- src/reactivity/tests/ref.spec.ts
+++ src/reactivity/tests/ref.spec.ts
@@ -7,7 +7,7 @@ describe("ref", () => {
     expect(a.value).toBe(1);
   });
   
-  it.only("should be reactive", () => {
+  it("should be reactive", () => {
     const a = ref(1);
     let dummy;
     let calls = 0;
@@ -39,38 +39,38 @@ describe("ref", () => {
     expect(dummy).toBe(2);
   });
 
-  // it("proxyRefs", () => {
-  //   const user = {
-  //     age: ref(10),
-  //     name: "xiaohong",
-  //   };
-  //   const proxyUser = proxyRefs(user);
-  //   expect(user.age.value).toBe(10);
-  //   expect(proxyUser.age).toBe(10);
-  //   expect(proxyUser.name).toBe("xiaohong");
+  it("proxyRefs", () => {
+    const user = {
+      age: ref(10),
+      name: "xiaohong",
+    };
+    const proxyUser = proxyRefs(user);
+    expect(user.age.value).toBe(10);
+    expect(proxyUser.age).toBe(10);
+    expect(proxyUser.name).toBe("xiaohong");
 
-  //   (proxyUser as any).age = 20;
-  //   expect(proxyUser.age).toBe(20);
-  //   expect(user.age.value).toBe(20);
+    (proxyUser as any).age = 20;
+    expect(proxyUser.age).toBe(20);
+    expect(user.age.value).toBe(20);
 
-  //   proxyUser.age = ref(10);
-  //   expect(proxyUser.age).toBe(10);
-  //   expect(user.age.value).toBe(10);
-  // });
+    proxyUser.age = ref(10);
+    expect(proxyUser.age).toBe(10);
+    expect(user.age.value).toBe(10);
+  });
 
-  // it("isRef", () => {
-  //   const a = ref(1);
-  //   const user = reactive({
-  //     age: 1,
-  //   });
-  //   expect(isRef(a)).toBe(true);
-  //   expect(isRef(1)).toBe(false);
-  //   expect(isRef(user)).toBe(false);
-  // });
+  it("isRef", () => {
+    const a = ref(1);
+    const user = reactive({
+      age: 1,
+    });
+    expect(isRef(a)).toBe(true);
+    expect(isRef(1)).toBe(false);
+    expect(isRef(user)).toBe(false);
+  });
 
-  // it("unRef", () => {
-  //   const a = ref(1);
-  //   expect(unRef(a)).toBe(1);
-  //   expect(unRef(1)).toBe(1);
-  // });
+  it("unRef", () => {
+    const a = ref(1);
+    expect(unRef(a)).toBe(1);
+    expect(unRef(1)).toBe(1);
+  });
 });
