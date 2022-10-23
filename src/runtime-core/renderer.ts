1713eef9a5b13fc501b9db61b991a05a5605d327
lyk990 <1477435414@qq.com>
2022-10-23 13:59:50 +0800

    shapeFlags

--- src/runtime-core/renderer.ts
+++ src/runtime-core/renderer.ts
@@ -1,6 +1,6 @@
-import { isVNode } from "vue";
 import { isObject } from "../shared";
 import { createComponentInstance, setupComponent } from "./component";
+import { ShapeFlags } from "../shared/shapeFlags";
 
 export function render(vnode, container) {
   // 调用patch方法
@@ -13,21 +13,23 @@ function patch(vnode, container) {
   // 去处理组件
   //  TODO 判断是不是element类型
   // processELement()
-  if (typeof vnode.type === "string") {
+  const { shapeFlag } = vnode;
+  if (shapeFlag & ShapeFlags.ELEMENT) {
     processELement(vnode, container);
-  } else if (isObject(vnode.type)) {
+  } else if (shapeFlag & ShapeFlags.STATEFUL_COMPONENT) {
     processComponent(vnode, container);
   }
 }
+
 function processELement(vnode: any, container: any) {
   mountElement(vnode, container);
 }
 function mountElement(vnode: any, container: any) {
   const el = (vnode.el = document.createElement(vnode.type));
-  const { children } = vnode;
-  if (typeof children == "string") {
+  const { children, shapeFlag } = vnode;
+  if (shapeFlag & ShapeFlags.TEXT_CHILDREN) {
     el.textContent = children;
-  } else if (Array.isArray(children)) {
+  } else if (shapeFlag & ShapeFlags.ARRAY_CHILDREN) {
     // vnode
     mountChildren(vnode, el);
   }
@@ -56,12 +58,12 @@ function mountComponent(initialVNode: any, container: any) {
   setupRenderEffect(instance, initialVNode, container);
 }
 
-function setupRenderEffect(instance:any, initialVNode, container) {
+function setupRenderEffect(instance: any, initialVNode, container) {
   const { proxy } = instance;
   const subTree = instance.render.call(proxy);
 
   // vnode -> patch
   // vnode -> element -> mountElement
   patch(subTree, container);
-  initialVNode.el = subTree.el 
+  initialVNode.el = subTree.el;
 }
