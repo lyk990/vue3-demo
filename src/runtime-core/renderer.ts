675c5abb381a15927ea39a16d36bd5a6b7c2655b
lyk990 <1477435414@qq.com>
2022-10-26 23:31:42 +0800

    element props

--- src/runtime-core/renderer.ts
+++ src/runtime-core/renderer.ts
@@ -3,6 +3,7 @@ import { ShapeFlags } from "../shared/shapeFlags";
 import { Fragment, Text } from "./vnode";
 import { createAppAPI } from "./createApp";
 import { effect } from "../reactivity/effect";
+import { EMPTY_OBJ } from "../shared";
 
 export function createRenderer(options) {
   const {
@@ -62,6 +63,29 @@ export function createRenderer(options) {
   }
 
   function patchElement(n1, n2, container) {
+    const oldProps = n1.props || EMPTY_OBJ;
+    const newProps = n2.props || EMPTY_OBJ
+    const el = (n2.el = n1.el);
+    patchProps(el, oldProps, newProps);
+  }
+  
+  function patchProps(el, oldProps, newProps) {
+    if (oldProps !== newProps) {
+      for (const key in newProps) {
+        const prevProp = oldProps[key];
+        const nextProp = newProps[key];
+        if (prevProp !== nextProp) {
+          hostPatchProp(el, key, prevProp, nextProp);
+        }
+      }
+      if (oldProps !== EMPTY_OBJ) {
+        for (const key in oldProps) {
+          if (!(key in newProps)) {
+            hostPatchProp(el, key, oldProps[key], null);
+          }
+        }
+      }
+    }
   }
 
   function mountElement(vnode: any, container: any, parentComponent) {
@@ -78,7 +102,7 @@ export function createRenderer(options) {
     const { props } = vnode;
     for (const key in props) {
       const val = props[key];
-      hostPatchProp(el, key, val);
+      hostPatchProp(el, key, null, val);
     }
     // container.append(el);
     hostInsert(el, container);
