2041287061d916271c15a79ee9d613b137420acd
lyk990 <1477435414@qq.com>
2022-10-30 00:17:21 +0800

    解析text

---
+++ src/compiler-core/src/parse.ts
@@ -0,0 +1,111 @@
+import { NodeTypes } from "./ast";
+
+const enum TagType {
+  Start,
+  End,
+}
+
+export function baseParse(content: string) {
+  const context = createParserContext(content);
+  return createRoot(parseChildren(context));
+}
+
+function parseChildren(context) {
+  const nodes: any = [];
+  let node;
+  const s = context.source;
+  if (s.startsWith("{{")) {
+    node = parseInterpolation(context);
+  } else if (s[0] === "<") {
+    if (/[a-z]/i.test(s[1])) {
+      node = parseElement(context);
+    }
+  }
+  if (!node) {
+    node = parseText(context);
+  }
+  nodes.push(node);
+  return nodes;
+}
+
+function parseText(context: any) {
+  // 获取content
+  const content = parseTextData(context, context.source.length);
+  return {
+    type: NodeTypes.TEXT,
+    content,
+  };
+}
+
+function parseTextData(context: any, length) {
+  const content = context.source.slice(0, length);
+  // 推进
+  advanceBy(context, length);
+  return content;
+}
+
+function parseElement(context: any) {
+  // 1.解析tag
+  const element = parseTag(context, TagType.Start);
+  parseTag(context, TagType.End);
+
+  return element;
+}
+
+function parseTag(context: any, type: TagType) {
+  const match: any = /^<\/?([a-z]*)/i.exec(context.source);
+  const tag = match[1];
+  // 2.删除处理完成的代码
+  advanceBy(context, match[0].length);
+  advanceBy(context, 1);
+  if (type === TagType.End) return;
+  return {
+    type: NodeTypes.ELEMENT,
+    tag,
+  };
+}
+
+function parseInterpolation(context) {
+  // {{message}}
+  const openDelimiter = "{{";
+  const closeDelimiter = "}}";
+
+  const closeIndex = context.source.indexOf(
+    closeDelimiter,
+    openDelimiter.length
+  );
+
+  advanceBy(context, openDelimiter.length);
+
+  const rawContentLength = closeIndex - openDelimiter.length;
+
+  const rawcontent = parseTextData(context, rawContentLength);
+
+  const content = rawcontent.trim();
+
+  advanceBy(context, closeDelimiter.length);
+
+  return {
+    type: NodeTypes.INTERPOLATION,
+    content: {
+      type: NodeTypes.SIMPLE_EXPRESSION,
+      content: content,
+    },
+  };
+}
+
+function advanceBy(context: any, length: number) {
+  context.source = context.source.slice(length);
+}
+
+function createRoot(children) {
+  return {
+    children,
+  };
+}
+
+function createParserContext(content: string): any {
+  return {
+    source: content,
+  };
+}
--- src/shared/test.ts
+++
@@ -1,6 +0,0 @@
-const ShapeFlags = {
-  element: 0,
-  statefule_component: 0,
-  text_children: 0,
-  array_children: 0,
-};
