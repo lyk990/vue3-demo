47691ad3f9a7e506de5fb318fc91d2abcc287590
lyk990 <1477435414@qq.com>
2022-10-30 23:42:08 +0800

    编译生成render

--- src/runtime-core/renderer.ts
+++ src/runtime-core/renderer.ts
@@ -336,9 +336,11 @@ export function createRenderer(options) {
     instance.update = effect(
       () => {
         if (!instance.isMounted) {
-          console.log("init");
           const { proxy } = instance;
-          const subTree = (instance.subTree = instance.render.call(proxy));
+          const subTree = (instance.subTree = instance.render.call(
+            proxy,
+            proxy
+          ));
           // vnode -> patch
           // vnode -> element -> mountElement
           patch(null, subTree, container, instance, anchor);
@@ -353,7 +355,7 @@ export function createRenderer(options) {
             updateComponentPreRender(instance, next);
           }
           const { proxy } = instance;
-          const subTree = instance.render.call(proxy);
+          const subTree = instance.render.call(proxy, proxy);
           const prevSubTree = instance.subTree;
           instance.subTree = subTree;
           patch(prevSubTree, subTree, container, instance, anchor);
